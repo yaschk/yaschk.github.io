3497f9f8dc5c6916fcaaca17dcb394dee1de8cbf
Olexandr Borysenko <tok29@i.ua>
2023-04-14 13:36:36 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+let tg = window.Telegram.WebApp;
+
+tg.expand();
+
+tg.MainButton.textColor = "#FFFFFF";
+tg.MainButton.color = "#5e8d4d";
+
+let item = "";
+
+let btn1 = document.getElementById("btn1");
+
+btn1.addEventListener("click", function (){
+    if (tg.MainButton.isVisible){
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Кнопка");
+        item = "1";
+        tg.MainButton.show();
+    }
+});
+
+Telegram.webapp.onEvent("mainButtonClicked", function (){
+    tg.sendData(item);
+});
+
