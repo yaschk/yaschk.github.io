2b4a4df2a54207a245bb09aaef830dc048619149
Olexandr Borysenko <tok29@i.ua>
2023-04-14 13:46:08 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-console.log(1)
+
 let tg = window.Telegram.WebApp;
 
 tg.expand();
@@ -9,12 +9,15 @@ tg.MainButton.color = "#5e8d4d";
 let item = "";
 
 let btn1 = document.getElementById("btn1");
+console.log(btn1)
 
 btn1.addEventListener("click", function (){
     if (tg.MainButton.isVisible){
+        console.log("Hide")
         tg.MainButton.hide();
     }
     else {
+        console.log("Show")
         tg.MainButton.setText("Кнопка");
         item = "1";
         tg.MainButton.show();
